bc9a3d552a7ec002cd93e66ce31cb39e490d1bf5
silvamarcio <silva.marcio86@hotmail.com>
2019-04-16 09:48:31 -0300

    Add project files.

---
+++ StudentsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
